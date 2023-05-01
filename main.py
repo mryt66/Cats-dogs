e656d0aadbf6e4ad392b1e0e45855ed7fcb86703
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-02 00:45:41 +0200

    Normalization of one .wav file

---
+++ main.py
@@ -0,0 +1,18 @@
+import wave
+import numpy
+
+#reading from file
+path=r'C:\Users\Marcin\Desktop\Projekty\SSI_cat_dog\archive\cats_dogs\train\cat\cat_1.wav'
+ifile= wave.open(path)
+samples=ifile.getnframes()
+audio=ifile.readframes(samples)
+
+#converting with numpy
+audio_int=numpy.frombuffer(audio, dtype=numpy.int16)
+audio_float=audio_int.astype(numpy.float32)
+
+#Normalization float to -1.0 : +1.0 range
+maxint=2**15
+audio_normalised=audio_float/maxint
+for i in audio_normalised:
+    print(i)
