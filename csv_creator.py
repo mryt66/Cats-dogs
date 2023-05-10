87257ccf6a410cf8ec848fd22e2780e5e1415ffb
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-10 12:39:36 +0200

    Zfiltrowane wartości do >0.0001 (żeby nie było danych co nie ma kota/psa)

--- csv_creator.py
+++ csv_creator.py
@@ -6,9 +6,8 @@ wav_files=[]
 #zapis 10 znormalziwoanych danych kotów do pliku csv 
 with open('Data_cats_10.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    for file in range(1,10):
-        path = rf'D:\Projekty\SSI_cat_dog\archive\checked_dataset\cat_0{file}.wav'
+    for file in range(1,3):
+        path = f'cat_0{file}.wav'
         y, sr = librosa.load(path)
+        [e for e in y if abs(e)>0.0001]
         writer.writerow(y) #zapis do pliku
-
-
