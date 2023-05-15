ca92800ae035668ae5fb29f5d6192a446aeb3d68
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-15 20:23:00 +0200

    Update csv_creator.py

--- csv_creator.py
+++ csv_creator.py
@@ -1,13 +1,28 @@
-import csv
-import librosa
+import numpy as np
+import soundfile as sf
+from scipy.io import wavfile
 
-wav_files=[]
+# Wczytanie pliku audio
+audio_data, fs = sf.read('cat_1.wav')
 
-#zapis 10 znormalziwoanych danych kotów do pliku csv 
-with open('Data_cats_10.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    for file in range(1,3):
-        path = f'cat_0{file}.wav'
-        y, sr = librosa.load(path)
-        [e for e in y if abs(e)>0.0001]
-        writer.writerow(y) #zapis do pliku
+# Obliczenie transformaty Fouriera
+fft = np.fft.fft(audio_data)
+
+
+# Obliczenie amplitud widmowych
+amplitudes = np.abs(fft)
+
+# Obliczenie częstotliwości próbkowania
+dt = 1/fs
+
+# Obliczenie wartości częstotliwości odpowiadającej każdemu elementowi w FFT
+freqs = np.fft.fftfreq(len(fft), dt)
+bands = np.array_split(np.arange(len(freqs)), 1000)
+#dodanie średnich amplitud do listy mean_ampltiude
+mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
+sorted_indices = np.argsort(mean_amplitude)[::-1]
+print(sorted_indices) 
+#Indeksy z najwyższymi średnimi amplitudami dla 1000 widm,
+#czyli warto brać pod uwagę pierwsze n widm zwłaszcza jak są blisko siebie tzn. z rzędu
+#w tym przypadku warto wziąć pod uwagę np. widma od 260 do 270 każde widmo to jakieś 30-60 danych w zależnośći od długości dźwięku
+#czyli z 40k danych pracujemy tylko na np. 300 danych dla każdego dźwięku co jest chyba dobrą opcją.
