725e3ebf40053619999858fbcc460b77331fa329
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-22 19:00:28 +0200

    Stworzenie i zapis danych do pliku csv

--- csv_creator.py
+++ csv_creator.py
@@ -2,27 +2,56 @@ import numpy as np
 import soundfile as sf
 from scipy.io import wavfile
 
-# Wczytanie pliku audio
-audio_data, fs = sf.read('cat_1.wav')
 
-# Obliczenie transformaty Fouriera
-fft = np.fft.fft(audio_data)
+class dog_cat:
+    def __init__(self, data):
+        self.data=data
+        self.audio_data=None
+        self.fs=None
+        
+    def read_data(self):
+        self.audio_data, self.fs = sf.read(f'cat_1-00{self.data}.wav')
+        
+    def create_data(self):
+        if self.audio_data is None:
+            raise ValueError("Brak danych")
+        # Obliczenie transformaty Fouriera
+        fft = np.fft.fft(self.audio_data)
 
+        # Obliczenie amplitud widmowych
+        amplitudes = np.abs(fft)
 
-# Obliczenie amplitud widmowych
-amplitudes = np.abs(fft)
+        #divide_number to liczba na ile widm chcemy podzielić nasz dźwięk
+        divide_number=1000 
+        multiplier=len(amplitudes)/divide_number
 
-# Obliczenie częstotliwości próbkowania
-dt = 1/fs
+        # Obliczenie częstotliwości próbkowania
+        dt = 1/self.fs
 
-# Obliczenie wartości częstotliwości odpowiadającej każdemu elementowi w FFT
-freqs = np.fft.fftfreq(len(fft), dt)
-bands = np.array_split(np.arange(len(freqs)), 1000)
-#dodanie średnich amplitud do listy mean_ampltiude
-mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
-sorted_indices = np.argsort(mean_amplitude)[::-1]
-print(sorted_indices) 
-#Indeksy z najwyższymi średnimi amplitudami dla 1000 widm,
-#czyli warto brać pod uwagę pierwsze n widm zwłaszcza jak są blisko siebie tzn. z rzędu
-#w tym przypadku warto wziąć pod uwagę np. widma od 260 do 270 każde widmo to jakieś 30-60 danych w zależnośći od długości dźwięku
-#czyli z 40k danych pracujemy tylko na np. 300 danych dla każdego dźwięku co jest chyba dobrą opcją.
+        # Obliczenie wartości częstotliwości odpowiadającej każdemu elementowi w FFT
+        freqs = np.fft.fftfreq(len(fft), dt)
+        bands = np.array_split(np.arange(len(freqs)), divide_number) 
+
+        #dodanie średnich amplitud do listy mean_ampltiude
+        mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
+
+        #posortowanie ich tak, żeby znaleźć te o najwyższej amplitudzie 
+        sorted_indices = np.argsort(mean_amplitude)[::-1]
+
+        target_amplitudes=np.array([])
+        for i in bands[sorted_indices[0]]:
+            target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
+
+        #target to jest tablica na której pracujemy i tu są dane o amplitudach z względem (nwm jakim na częstotliwości)
+        print(target_amplitudes)
+
+        #zapis do pliku w 1 linijce tablicy target
+        with open('Data_cats_10.csv', 'a') as file:
+            np.savetxt(file, [target_amplitudes], fmt='%.8f', delimiter=',')
+            
+#Wczytanie audio
+for iterator in range(1,9):
+    thing=dog_cat(iterator)
+    thing.read_data()
+    thing.create_data()
+    
