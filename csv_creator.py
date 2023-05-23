28a3921bfb41953f32d9b734982ae907e66eae06
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-23 19:35:09 +0200

    Dobre czytanie z pliku i zapisywanie do listy

--- csv_creator.py
+++ csv_creator.py
@@ -1,57 +1,75 @@
 import numpy as np
+import pandas as pd
 import soundfile as sf
+import csv
+from sklearn.model_selection import train_test_split
 from scipy.io import wavfile
 
 
 class dog_cat:
-    def __init__(self, data):
-        self.data=data
+    
+    def __init__(self, type, number):
+        self.type=type
+        self.number=number
         self.audio_data=None
         self.fs=None
         
     def read_data(self):
-        self.audio_data, self.fs = sf.read(f'cat_1-00{self.data}.wav')
+        path=f'{self.type}_1-00{self.number}.wav'
+        self.audio_data, self.fs = sf.read(path)
         
     def create_data(self):
         if self.audio_data is None:
             raise ValueError("Brak danych")
-        # Obliczenie transformaty Fouriera
+
         fft = np.fft.fft(self.audio_data)
-
-        # Obliczenie amplitud widmowych
         amplitudes = np.abs(fft)
-
-        #divide_number to liczba na ile widm chcemy podzielić nasz dźwięk
-        divide_number=1000 
+        
+        divide_number=1000
         multiplier=len(amplitudes)/divide_number
 
-        # Obliczenie częstotliwości próbkowania
         dt = 1/self.fs
 
-        # Obliczenie wartości częstotliwości odpowiadającej każdemu elementowi w FFT
         freqs = np.fft.fftfreq(len(fft), dt)
         bands = np.array_split(np.arange(len(freqs)), divide_number) 
 
-        #dodanie średnich amplitud do listy mean_ampltiude
         mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
-
-        #posortowanie ich tak, żeby znaleźć te o najwyższej amplitudzie 
         sorted_indices = np.argsort(mean_amplitude)[::-1]
 
         target_amplitudes=np.array([])
         for i in bands[sorted_indices[0]]:
             target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
 
-        #target to jest tablica na której pracujemy i tu są dane o amplitudach z względem (nwm jakim na częstotliwości)
-        print(target_amplitudes)
-
-        #zapis do pliku w 1 linijce tablicy target
+        df=pd.DataFrame({'Amplitudes': [target_amplitudes]})
         with open('Data_cats_10.csv', 'a') as file:
-            np.savetxt(file, [target_amplitudes], fmt='%.8f', delimiter=',')
-            
-#Wczytanie audio
-for iterator in range(1,9):
-    thing=dog_cat(iterator)
-    thing.read_data()
-    thing.create_data()
+            file.write(df.to_string(header=False, index=False))
+            file.write('\n')
+    
+    
+# Stworzenie zbioru treningowego dla kotów
+# for iterator in range(1,9):
+#     thing=dog_cat('cat',iterator)
+#     thing.read_data()
+#     thing.create_data()
+
+data_cats=[]
+with open('Data_cats_10.csv', 'r', newline='') as file:
+    reader=csv.reader(file, quoting=csv.QUOTE_NONE)
+    for i in reader:
+        tmp=[]
+        for j in i:
+            if j[0]=='[':
+                j=j.strip('[')
+            elif j[len(j)-1]==']':
+                j=j.strip(']')
+            tmp.append(j)
+        data_cats.append(tmp)
+for i in data_cats:
+    print(i)
+
+
+#co zrobić !!!
+#dorobić zabezpieczenie, żeby nie było dwóch tych samych linijek w .csv     
+#dorobić tak, żeby otwierać wszystkie pliki w klasie a nastepnie zapisywać całość data setu
+    
     
