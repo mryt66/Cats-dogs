6ff720103a7efc8b9d9a1a705117ce2947904365
Marcin <64143856+mryt66@users.noreply.github.com>
2023-06-01 13:28:05 +0200

    Update csv_creator.py

--- csv_creator.py
+++ csv_creator.py
@@ -1,58 +1,78 @@
 import numpy as np
 import pandas as pd
 import soundfile as sf
-import csv
-from sklearn.model_selection import train_test_split
+import random
+from statistics import mean
 from scipy.io import wavfile
 
-class dog_cat:
+data=[]
+
+def load_csv(file, type):
+    with open(file, newline='') as file:
+        reader=csv.reader(file, quoting=csv.QUOTE_NONE)
+        for i in reader:
+            tmp=[]
+            for j in i:
+                if j[0]=='[':
+                    j=float(j.strip('['))
+                elif j[len(j)-1]==']':
+                    j=float(j.strip(']'))
+                else:
+                    j=float(j)
+                tmp.append(j)
+            
+            dl=1
+            num=0
+            for j in tmp:
+                dl=dl+1
+                num+=float(j)
+            if dl>5:
+                data.append([mean(tmp), type, dl-1, min(tmp), max(tmp)])
+    return data
+
+load_csv('Data_cats.csv','cats')
+load_csv('Data_dogs.csv','dogs')
+
+class load_data:
     
-    def __init__(self, type, number, path):
-        self.type=type
-        self.number=number
-        self.audio_data=None
-        self.fs=None
-        self.path=path
+    def __init__(self, data):
+        self.data=data
         
-    def read_data(self):
-        path=f'only_once_miau_and_hał_data/{self.type}_{self.number}.wav'
-        self.audio_data, self.fs = sf.read(path)
-        
-    def create_data(self):
-        if self.audio_data is None:
-            raise ValueError("Brak danych")
-
-        fft = np.fft.fft(self.audio_data)
-        amplitudes = np.abs(fft)
-        
-        divide_number=1000
-        multiplier=len(amplitudes)/divide_number
-        dt = 1/self.fs
-
-        freqs = np.fft.fftfreq(len(fft), dt)
-        bands = np.array_split(np.arange(len(freqs)), divide_number) 
-        mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
-        sorted_indices = np.argsort(mean_amplitude)[::-1]
-
-        target_amplitudes=np.array([])
-        for i in bands[sorted_indices[0]]:
-            target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
-        df=pd.DataFrame({'Amplitudes': [target_amplitudes]})
-        with open(self.path, 'a') as file:
-            file.write(df.to_string(header=False, index=False))
-            file.write('\n')
+    def shuffle(self):
+        for i in range(len(self.data) - 1, 0, -1):
+            j = random.randint(0, i)
+            self.data[i], self.data[j] =  self.data[j], self.data[i]
+        return self.data
     
-   
-# for iterator in range(11,25):
-#     thing=dog_cat('cat',iterator, 'Data_cats.csv')
-#     thing.read_data()
-#     thing.create_data()
+    def split(self):
+        split=int(len(self.data)*0.9)
+        listTrain = self.data[0:split]
+        listVal = self.data[split: len(self.data)]
+        return listTrain, listVal
 
+            
+x=load_data(data)
+x.shuffle()
+train,val=x.split()
 
+k=3
 
+print(train)
+
+#całki
+#średnia całkowa, plot
+#średnią, min,max,std,ilość
+
+# train_data, test_data = train_test_split(data_cats, test_size=0.1, random_state=42)
+
+# scaler = StandardScaler()
+# X_train_scaled = scaler.fit_transform(train_cats)
+# X_test_scaled = scaler.transform(X_test)
+
+# knn = KNeighborsClassifier(n_neighbors=5)
 
 # data_cats=[]
-# with open('Data_cats_10.csv', 'r', newline='') as file:
+# with open(csv_cats, 'r', newline='') as file:
 #     reader=csv.reader(file, quoting=csv.QUOTE_NONE)
 #     for i in reader:
 #         tmp=[]
@@ -65,10 +85,3 @@ class dog_cat:
 #         data_cats.append(tmp)
 # for i in data_cats:
 #     print(i)
-
-
-
-#co zrobić !!!
-#dorobić zabezpieczenie, żeby nie było dwóch tych samych linijek w .csv     
-    
-    
