4ef3e264fcd36b08f89520c7e32c232340cbcba2
Marcin <64143856+mryt66@users.noreply.github.com>
2023-06-01 19:05:48 +0200

    Update csv_creator.py

--- csv_creator.py
+++ csv_creator.py
@@ -13,22 +13,31 @@ class dog_cat:
         self.fs=None
         self.path=path
 
-    def read_data(self):
-        path=f'{self.type}_{self.number}.wav'
+    def read_wave(self):
         path=f'only_once_miau_and_hał_data/{self.type}_{self.number}.wav'
         self.audio_data, self.fs = sf.read(path)
-
+    
     def create_data(self):
         if self.audio_data is None:
             raise ValueError("Brak danych")
+            
+        fft = np.fft.fft(self.audio_data)
+        amplitudes = np.abs(fft)
 
-	def create_data(self, path):
+        divide_number=1000
+        multiplier=len(amplitudes)/divide_number
+        dt = 1/self.fs
+        
+        freqs = np.fft.fftfreq(len(fft), dt)
+
+        bands = np.array_split(np.arange(len(freqs)), divide_number) 
+        mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
+        sorted_indices = np.argsort(mean_amplitude)[::-1]
+        
         target_amplitudes=np.array([])
         for i in bands[sorted_indices[0]]:
             target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
-
         df=pd.DataFrame({'Amplitudes': [target_amplitudes]})
-        with open(path, 'a') as file:
         with open(self.path, 'a') as file:
             file.write(df.to_string(header=False, index=False))
             file.write('\n')
