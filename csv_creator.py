df4860d06b25190418656924f2507395adc96570
Marcin <64143856+mryt66@users.noreply.github.com>
2023-06-02 02:46:39 +0200

    Update csv_creator.py

--- csv_creator.py
+++ csv_creator.py
@@ -14,7 +14,7 @@ class dog_cat:
         self.path=path
 
     def read_wave(self):
-        path=f'only_once_miau_and_hał_data/{self.type}_{self.number}.wav'
+        path=f'cats&dogs .wav/{self.type}_{self.number:02d}.wav'
         self.audio_data, self.fs = sf.read(path)
     
     def create_data(self):
@@ -38,12 +38,7 @@ class dog_cat:
         for i in bands[sorted_indices[0]]:
             target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
         df=pd.DataFrame({'Amplitudes': [target_amplitudes]})
-        with open(self.path, 'a') as file:
+        with open(self.path, 'a') as file:         
             file.write(df.to_string(header=False, index=False))
             file.write('\n')
 
-
-#całki
-#średnia całkowa, plot
-#średnią, min,max,std,ilość
-# train_data, test_data = train_test_split(data_cats, test_size=0.1, random_state=42)
