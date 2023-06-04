75aa07dba09ab3a977a36801880c595f4f7c5fe6
Marcin <64143856+mryt66@users.noreply.github.com>
2023-06-04 17:05:36 +0200

    creator and main

---
+++ src/csv_creator.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import soundfile as sf
+import csv
+from scipy.io import wavfile
+
+class DogCat:
+    
+    def __init__(self, type, number, path):
+        self.type = type
+        self.number = number
+        self.audio_data = None
+        self.fs = None
+        self.path = path
+
+    def read_wave(self):
+        path=f'cats&dogs .wav/{self.type}_{self.number:02d}.wav'
+        self.audio_data, self.fs = sf.read(path)
+    
+    def create_data(self):
+        if self.audio_data is None:
+            raise ValueError("No data")
+            
+        fft = np.fft.fft(self.audio_data)
+        amplitudes = np.abs(fft)
+        multiplier = len(amplitudes)/1000
+        dt = 1/self.fs
+        
+        freqs = np.fft.fftfreq(len(fft), dt)
+
+        bands = np.array_split(np.arange(len(freqs)), 1000) 
+        mean_amplitude = [np.mean(amplitudes[band]) for band in bands] 
+        sorted_indices = np.argsort(mean_amplitude)[::-1]
+        
+        target_amplitudes=np.array([])
+        for i in bands[sorted_indices[0]]:
+            target_amplitudes=np.append(target_amplitudes,amplitudes[i][0])
+        
+        df = pd.DataFrame([target_amplitudes])
+        df.to_csv(self.path, index=False, header=False, mode='a')
